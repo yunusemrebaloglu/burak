d901dcf79c32395ebe9101ab290ec3155b3bae31
Yunus Emre Baloğlu <yunus@yunusemre.co>
2024-07-23 09:20:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Ben artık PHP yazıyorum.";
+$name = "Burak";
+$data = "PHP'ye uzanan eller kırılsın. PHP'ye laf etmez olaydım. Çok pişmanım. :(";
+$result = $data." Yazan:".$name;
+echo "<h1>".$result."</h1>";
+die();
+
+
+?>
